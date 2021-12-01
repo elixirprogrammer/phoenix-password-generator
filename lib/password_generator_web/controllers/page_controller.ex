ad0cfd3471175c490472b591f4912dad6e768324
Anthony Gonzalez <boilercoding@gmail.com>
2021-12-01 04:42:44 -0500

    adds template styles

--- lib/password_generator_web/controllers/page_controller.ex
+++ lib/password_generator_web/controllers/page_controller.ex
@@ -2,6 +2,19 @@ defmodule PasswordGeneratorWeb.PageController do
   use PasswordGeneratorWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    password_lengths = [
+      Weak: Enum.map(6..15, & &1),
+      Strong: Enum.map(16..88, & &1),
+      Unbelievable: [100, 150]
+    ]
+
+    password = ""
+
+    render(
+      conn,
+      "index.html",
+      password_lengths: password_lengths,
+      password: password
+    )
   end
 end
